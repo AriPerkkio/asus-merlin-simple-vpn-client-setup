f77cecf1be10dcd32a8c81fc0385cdb3267fa84e
Ari Perkkio <ari.perkkio@gmail.com>
2020-01-05 20:47:01 +0200

    Bundle API

---
+++ packages/api/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js',
+    },
+    target: 'node',
+    optimization: {
+        minimize: false,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.mjs', '.js'],
+        modules: ['node_modules', './'],
+    },
+};
